336e9609b6417135efb30d539b008647cfd77ba7
Bintina <105106238+Bintina@users.noreply.github.com>
2023-11-09 21:40:46 +0300

    Preliminary xml
    Experimenting with elements to put together the Add Search screen.

--- app/src/main/java/com/bintina/mynews/topstories/adapter/Adapter.kt
+++ app/src/main/java/com/bintina/mynews/topstories/adapter/Adapter.kt
@@ -26,7 +26,6 @@ class Adapter() : RecyclerView.Adapter<Adapter.ItemViewHolder>() {
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         holder.bind(storiesList[position], listener)
-
     }
     class ItemViewHolder(private val view: ItemRowBinding) : RecyclerView.ViewHolder(view.root) {
         fun bind(news: News?, listener: OnNewsClickedListener) {
