c941a5fd2749a623f1d8faa5bca02c0c2a8860c7
Bintina <105106238+Bintina@users.noreply.github.com>
2024-02-01 18:35:54 +0300

    SearchActivityUnitTest initializer commented out

--- app/src/test/java/com/bintina/mynews/SearchActivityUnitTest.kt
+++ app/src/test/java/com/bintina/mynews/SearchActivityUnitTest.kt
@@ -10,12 +10,13 @@ import org.junit.runners.JUnit4
 @RunWith(JUnit4::class)
 class SearchActivityUnitTest {
 
+   /*
     @Rule
     @JvmField
     val rule: ActivityScenarioRule<SearchActivity> =
         ActivityScenarioRule(SearchActivity::class.java)
 
-   /* @Test
+   @Test
     fun check_that_the_query_is_formatted_correctly() {
         TODO("check that urlQuerySanitizer is implemented correctly once applied")
     }
