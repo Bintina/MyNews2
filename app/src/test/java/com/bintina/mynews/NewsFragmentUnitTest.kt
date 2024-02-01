dc5afc00fd0b269350ab15392ee193f0546711b5
Bintina <105106238+Bintina@users.noreply.github.com>
2024-02-01 18:10:48 +0300

    DataSourceUnitTest SearchActivityUnitTest and NewsFragmentUnitTest Placeholders added.

---
+++ app/src/test/java/com/bintina/mynews/NewsFragmentUnitTest.kt
@@ -0,0 +1,20 @@
+package com.bintina.mynews
+
+class NewsFragmentUnitTest {
+/*
+    @Mock
+    lateinit var mockDataSource: DataSource*/
+//convert response to object
+
+    class mockNewsDataSource(
+//overide loadNews to manually convert mock data
+    ){
+    }
+//AS IS, before refactoring.
+    //mock successful list
+    //......without filters:
+    //mock empty list result(no response) should return null
+    //mock mixed list
+
+    //DataSource test filters
+}
---
+++ app/src/test/java/com/bintina/mynews/SearchActivityUnitTest.kt
@@ -0,0 +1,24 @@
+package com.bintina.mynews
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.bintina.mynews.search.controller.SearchActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class SearchActivityUnitTest {
+
+    @Rule
+    @JvmField
+    val rule: ActivityScenarioRule<SearchActivity> =
+        ActivityScenarioRule(SearchActivity::class.java)
+
+    @Test
+    fun check_that_the_query_is_formatted_correctly() {
+        TODO("check that urlQuerySanitizer is implemented correctly once applied")
+    }
+
+
+}
